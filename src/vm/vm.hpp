0f9d589c774355d1103a8f73fae93037563a9991
Liamwaaga <liamwaaga@proton.me>
2026-02-16 14:36:35 -0800

    add license text to all source and header files

--- src/vm/vm.hpp
+++ src/vm/vm.hpp
@@ -1,3 +1,26 @@
+/*
+ * Simple modular 6502 emulator written in c++
+ * Copyright (C) 2026 N Liam Waaga
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
 #pragma once
 
 
@@ -11,15 +34,15 @@ class VirtualMemory {
     public:
     VirtualMemory();
     ~VirtualMemory();
-    
+
     void addDevice(Device dev);
-    
+
     Byte read_byte(Word address);
     Word read_word(Word address);
-    
+
     void write_byte(Word address, Byte value);
     void write_word(Word address, Word value);
-    
+
     private:
     std::vector<Device> devices;
 };
